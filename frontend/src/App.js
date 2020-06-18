f630a29f321af71401a1058dee711c38247002fb
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-06-18 19:36:46 -0300

    props concept (Propriedade)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,12 @@ import React from 'react';
 import Header from './components/Header.js';
 
 function App(){
-  return <Header />
+  return (
+    <div>
+      <Header title="Homepage" />
+      <Header title="Projects" />
+    </div>
+  )
 }
 
 export default App;
