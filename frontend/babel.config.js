e694bc647eb9052517ca3fc0a55addd2b8adf032
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-06-17 21:34:41 -0300

    added babel config

---
+++ frontend/babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    /* Converte o código de js moderno para js mais antigo de acordo 
+    com o ambiente que esta rodando o código*/
+    '@babel/preset-env',
+    /* Adiciona as funcionalidades do react na conversão do babel*/
+    '@babel/preset-react'
+  ],
+}
