cad0cb31985130226b1dbeb14d6f95fc3beaf918
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-06-25 19:38:21 -0300

    added FlatList replacing View

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
 
 import api from './services/api';
 
@@ -22,11 +22,21 @@ export default function App(){
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
+      
+      <FlatList
+        style={styles.container}
+        data={projects}
+        keyExtractor={project => project.id}
+        renderItem={({ item }) => (
+          <Text style={styles.project}>{item.title}</Text>
+        )}
+      />
+
+      {/*<View style={styles.container}>
         {projects.map(project => (
           <Text style={styles.project} key={project.id}>{project.title}</Text>
         ))}
-      </View>
+      </View> */}
     </>
   );
 }
@@ -35,8 +45,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
   project: {
     color: '#FFF',
