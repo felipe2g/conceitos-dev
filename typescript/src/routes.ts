785198e71e5c59fe9b52d0e3d2695ffa287b985d
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-06-28 09:43:56 -0300

    added interface to type array

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -9,6 +9,12 @@ export function helloWorld(request: Request, response: Response) {
   const user = createUser({
     email: 'felipe@email.com',
     password: '123456',
+    techs: [
+      'Node.js', 
+      'ReactJS', 
+      'React Native',
+      { title: 'Javascript', experience: 100},
+    ]
   });
 
   return response.json({ message: 'Hello World' });
--- typescript/src/services/CreateUser.ts
+++ typescript/src/services/CreateUser.ts
@@ -2,10 +2,16 @@
  * PAra criar: name, email, password
  */
 
+interface TechObject {
+  title: string;
+  experience: number;
+}
+
 interface CreateUserData {
   name?: string;
   email: string;
   password: string;
+  techs: Array<string | TechObject>;
 }
 
 export default function createUser({ name, email, password }: CreateUserData) {
