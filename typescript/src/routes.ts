7a127b85323a2352cb424026bdaf290319d2f939
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-06-28 09:39:38 -0300

    added interface to type object

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+// string, number, boolean, object, array
+// interface = tipagem de conjunto de dados
 
 export function helloWorld(request: Request, response: Response) {
-    return response.json({ message: 'Hello World' });
+
+  const user = createUser({
+    email: 'felipe@email.com',
+    password: '123456',
+  });
+
+  return response.json({ message: 'Hello World' });
 }
